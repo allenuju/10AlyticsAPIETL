91bedeff1a60dc44878db8c28202cf1f8443beca
allenuju <allenuju@gmail.com>
2023-03-14 00:58:24 +0000

    updated the code with an addition functionality

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 10AlyticsAPIETL
 This project contain the pipeline for extracting exchange rates data from XE API, transforming the data and load to a Postgresql Database
+`
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 def github_learning():
-   print ("welcome to Github")
+    # add two numbers
+    var1 = 5
+    var2 = 10
+    sum_of_num = var1 + var2
+    print(f"The sum of numbers is {sum_of_num }")
 
 github_learning()
